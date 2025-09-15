aa4c35d3bb558041363b463af9ce381f02cdd706
Falmnw <naufalwisaksono3112@gmail.com>
2025-09-15 22:31:18 +0700

    Tambahan icon pada tab browser

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'My App')</title>
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="icon" type="image/png" href="{{ asset('assets/images/logo-apk-simple.png') }}">
 </head>
 <body>
     <div class="container">
