ffccdc1d567a8d299a4c26ad3b18b5f3772eeae2
Falmnw <naufalwisaksono3112@gmail.com>
2025-09-23 09:04:49 +0700

    Fix nullable in users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nim')->unique();
-            $table->string('name');
-            $table->string('daerah');
-            $table->string('organisasi');
+            $table->string('nim')->unique()->nullable();
+            $table->string('name')->nullable();
+            $table->string('daerah')->nullable();
+            $table->string('organisasi')->nullable();
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('google_id')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('google_token')->nullable();
